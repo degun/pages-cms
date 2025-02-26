70111c5f04b3eebf140a270be6e796d84ba8090f
degun <edlir.velaj@outlook.com>
2025-02-26 18:19:37 +0100

    GITHUB_APP_PRIVATE_KEY

--- lib/token.ts
+++ lib/token.ts
@@ -11,6 +11,11 @@ import {
 import { and, eq, sql } from "drizzle-orm";
 import { User } from "@/types/user";
 
+const gh = `-----BEGIN RSA PRIVATE KEY-----
+${process.env.GITHUB_APP_PRIVATE_KEY}
+-----END RSA PRIVATE KEY-----
+`
+
 const getToken = cache(async (user: User, owner: string, repo: string) => {
   if (user.githubId) return await getUserToken();
 
@@ -30,7 +35,7 @@ const getToken = cache(async (user: User, owner: string, repo: string) => {
 const getInstallationToken = cache(async (owner: string, repo: string) => {
   const app = new App({
 		appId: process.env.GITHUB_APP_ID!,
-		privateKey: process.env.GITHUB_APP_PRIVATE_KEY!,
+		privateKey: gh!,
 	});
 
   const repoInstallation = await app.octokit.rest.apps.getRepoInstallation({ owner, repo });
