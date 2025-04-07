c49862d88f05bfd44415dc51b4c6e8669341d28d
degun <edlir.velaj@outlook.com>
2025-04-07 19:25:51 +0200

    remove react colorful

--- fields/custom/color-picker/edit-component.tsx
+++ fields/custom/color-picker/edit-component.tsx
@@ -2,17 +2,14 @@
 
 import { forwardRef } from "react";
 import React from "react";
-import { HexColorPicker } from "react-colorful";
+
+import "./edit-component.css";
 
 const EditComponent = forwardRef((props: any, ref: React.Ref<HTMLInputElement>) => {
   return (
-    <div className="flex flex-col items-center gap-4 p-4 rounded-2xl shadow-md w-fit bg-white" ref={ref}>
-      <HexColorPicker color={props.value} onChange={props.onChange} />
-      <div
-        className="w-16 h-16 rounded-full border"
-        style={{ backgroundColor: props.value }}
-      />
-      <p className="text-sm font-mono">{props.value}</p>
+    <div className="flex content-center items-center gap-2">
+      <input type="color" ref={ref} value={props.value} onChange={props.onChange} />
+      <input type="text" value={props.value} onChange={props.onChange} />
     </div>
   );
 });
